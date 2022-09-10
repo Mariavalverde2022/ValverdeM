ab1d5e55252f1e8e83e8c61fd86c267f15e03d2e
Maria Jose <mariajosevaoverdebautista@gmail.comgit config --global user.email mariajosevalverdebautista@gmail.com>
2022-09-10 11:47:54 -0500

    tercera version

--- suma.cpp
+++ suma.cpp
@@ -3,16 +3,19 @@ using namespace std;
 int main()
 
 {
-	cout<<"Ingrese 2 numero separados de espacio x y :";
+	
   float x,y,z;
-  cout<<"Ingrese 2 numero x y :";
+
+  cout<<"Ingrese 2 numero separados de espacio x y :";
+
   cin>>x>>y;
-  cout<<"El resultado es :";
+
   z=x+y;
+
   cout<<"El resultado es :";
 
-  cout<<z;
 cout<<z<<endl;
+
   return 0;
 }
 
